1823a076b97f72ec3bd87c323d54611a475484e7
rizaldidev <rizaldirnm@gmail.com>
2021-05-20 22:20:20 +0700

    adding config file

--- src/configuration.rs
+++ src/configuration.rs
@@ -14,3 +14,11 @@ pub struct DatabaseSettings {
   pub host: String,
   pub database_name: String,
 }
+
+pub fn get_configuration() -> Result<Settings, config::ConfigError> {
+  let mut settings = config::Config::default();
+
+  settings.merge(config::File::with_name("configuration"))?;
+
+  settings.try_into()
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,17 @@
 use std::net::TcpListener;
 
 use zero2prod::run;
+use zero2prod::configuration::get_configuration;
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
 
+    let configuration = get_configuration().expect("Failed to read configuration");
+
+    let addres = format!("127.0.0.1:{}", configuration.application_port);
+
     // Listen TCP in localhost:8000
-    let listener = TcpListener::bind("127.0.0.1:8000")
+    let listener = TcpListener::bind(addres)
         .expect("Failed to bind listener");
 
     run(listener)?.await
